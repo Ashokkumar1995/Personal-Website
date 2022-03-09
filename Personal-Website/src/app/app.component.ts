b9e30fa6193e1f522074bd63add0e6255242b270
Ashokkumar Ramasamy <ashokkumarindia10@gmail.com>
2022-03-09 23:59:02 +0000

    icons color changes

--- Personal-Website/src/app/app.component.ts
+++ Personal-Website/src/app/app.component.ts
@@ -27,41 +27,25 @@ export class AppComponent implements OnInit {
   modeTooltip = 'Turn On Dark Mode';
   constructor(private router: Router, private service: AppComponentService) {
     this.time = new Date().getHours();
-    // this.time = 11;
-    // if (this.time < 12) {
-    //   this.day = Day.light;
-    //   this.light = true;
-    //   this.service.changeTheme(this.day);
-    //   this.lightIcon = 'brightness_3';
-    // }
-    this.service.changeTheme(this.day);
+    if (this.time < 12) {
+      this.lightSettings();
+    } else {
+      this.darkSettings();
+    }
+
     this.iconsList.push(new Icons('Home', 'home'));
     this.iconsList.push(new Icons('About', 'person_outline'));
     this.iconsList.push(new Icons('Work History', 'work_outline'));
     this.iconsList.push(new Icons('Contact', 'chat'));
 
     this.router.events.subscribe((event) => {
-      // if (event instanceof NavigationStart) {
-      //     // Show progress spinner or progress bar
-      //     console.log('Route change detected');
-      // }
-
       if (event instanceof NavigationEnd) {
-        // Hide progress spinner or progress bar
-        // this.currentRoute = event.url;
         this.iconsList.forEach((icons: Icons) => {
-          // icons.isActive=false;
           icons.identifier === event.url.substring(1) ? (icons.isActive = true) : (icons.isActive = false);
         });
-        // this.iconsList.find(icon=>)
-        // console.log(event);
-        // console.log(this.iconsList);
       }
 
       if (event instanceof NavigationError) {
-        // Hide progress spinner or progress bar
-
-        // Present error to user
         this.iconsList.forEach((icons: Icons) => (icons.isActive = false));
         this.iconsList[0].isActive = true;
         console.log(event.error);
@@ -71,16 +55,22 @@ export class AppComponent implements OnInit {
 
   switchMode(): void {
     if (this.day == Day.light) {
-      this.service.changeTheme(Day.dark);
-      this.lightIcon = 'light';
-      this.light = false;
-      this.modeTooltip = 'Turn On Light Mode';
+      this.darkSettings();
     } else {
-      this.lightIcon = 'brightness_3';
-      this.service.changeTheme(Day.light);
-      this.light = true;
-      this.modeTooltip = 'Turn On Dark Mode';
+      this.lightSettings();
     }
+  }
+  private lightSettings(): void {
+    this.lightIcon = 'brightness_3';
+    this.service.changeTheme(Day.light);
+    this.light = true;
+    this.modeTooltip = 'Turn On Dark Mode';
+  }
+  private darkSettings(): void {
+    this.service.changeTheme(Day.dark);
+    this.lightIcon = 'light';
+    this.light = false;
+    this.modeTooltip = 'Turn On Light Mode';
   }
   ngOnInit(): void {
     this.subscription = this.service.currentTheme.subscribe((theme: Day) => (this.day = theme));
